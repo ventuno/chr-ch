0f87e33ee23a4b85635185ee452977a02e6725f7
ventuno <ernesto@fermi-7.local>
2014-11-15 01:46:22 -0800

    Base angular project for the client.

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
 var api = require('./routes/api');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -21,24 +21,40 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+//app.use('/users', users);
 app.use('/api', api);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
+});*/
 
 // error handlers
 
 // development error handler
 // will print stacktrace
+/*if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}*/
 if (app.get('env') === 'development') {
+    // This will change in production since we'll be using the dist folder
+    app.use(express.static(path.join(__dirname, '/ch-chr-client')));
+    // This covers serving up the index page
+    app.use(express.static(path.join(__dirname, '/ch-chr-client/.tmp')));
+    app.use(express.static(path.join(__dirname, '/ch-chr-client/app')));
+
+    // Error Handling
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -50,13 +66,28 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+/*app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
-});
+});*/
+if (app.get('env') === 'production') {
+
+    // changes it to use the optimized version for production
+    app.use(express.static(path.join(__dirname, '/dist')));
+
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+}
 
 
 module.exports = app;
