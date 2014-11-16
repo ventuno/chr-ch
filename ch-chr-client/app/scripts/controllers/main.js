0831375c6f8ac37423ce2b6f920d1e15c6c0c570
ventuno <ernesto@fermi-3.local>
2014-11-16 12:04:40 -0800

    [Client] Updating UI with additional filtering opt

--- ch-chr-client/app/scripts/controllers/main.js
+++ ch-chr-client/app/scripts/controllers/main.js
@@ -3,10 +3,12 @@
 angular.module('chChrClientApp')
   .controller('MainCtrl', ['$scope', 'TwitterProfile', 'TwitterTimeline', function ($scope, TwitterProfile, TwitterTimeline) {
   	$scope.selectedUser = "";
-  	$scope.numberOfTweets = 10;
+  	$scope.numberOfTweets;
   	$scope.startDateFilter = null;
   	$scope.endDateFilter = null;
   	$scope.selectedUserTweets = [];
+    $scope.picturesFilter = false;
+    $scope.minRetweets;
     $scope.fetchUserInfo = function () {
   		$scope.selectedUserProfile = TwitterProfile.get({
   			twitterhandle: $scope.selectedUser
@@ -16,7 +18,9 @@ angular.module('chChrClientApp')
   			twitterhandle: $scope.selectedUser,
   			count: $scope.numberOfTweets,
   			start_date: new Date($scope.startDateFilter).getTime() || 0,
-  			end_date: new Date($scope.endDateFilter).getTime() || 0
+  			end_date: new Date($scope.endDateFilter).getTime() || 0,
+        with_pictures_only: $scope.picturesFilter,
+        retweet_filter: $scope.minRetweets | 0
   		});
   	};
   }]);
