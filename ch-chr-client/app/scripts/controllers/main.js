b4197c61d233d05ddaf0b99476ae7c72eebcafd9
ventuno <ernesto@fermi-7.local>
2014-11-15 17:45:25 -0800

    [Client] Support date filtering on the UI side.

--- ch-chr-client/app/scripts/controllers/main.js
+++ ch-chr-client/app/scripts/controllers/main.js
@@ -3,10 +3,20 @@
 angular.module('chChrClientApp')
   .controller('MainCtrl', ['$scope', 'TwitterProfile', 'TwitterTimeline', function ($scope, TwitterProfile, TwitterTimeline) {
   	$scope.selectedUser = "";
-  	$scope.numberOfTweets;
+  	$scope.numberOfTweets = 10;
+  	$scope.startDateFilter = null;
+  	$scope.endDateFilter = null;
   	$scope.selectedUserTweets = [];
     $scope.fetchUserInfo = function () {
-  		$scope.selectedUserProfile = TwitterProfile.get({twitterhandle: $scope.selectedUser});
-  		$scope.selectedUserTweets = TwitterTimeline.get({twitterhandle: $scope.selectedUser, count: $scope.numberOfTweets});
+  		$scope.selectedUserProfile = TwitterProfile.get({
+  			twitterhandle: $scope.selectedUser
+  		});
+
+  		$scope.selectedUserTweets = TwitterTimeline.get({
+  			twitterhandle: $scope.selectedUser,
+  			count: $scope.numberOfTweets,
+  			start_date: new Date($scope.startDateFilter).getTime() || 0,
+  			end_date: new Date($scope.endDateFilter).getTime() || 0
+  		});
   	};
   }]);
