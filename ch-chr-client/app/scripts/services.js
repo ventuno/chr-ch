93ca8862e9d01a249f56a4b3ebd532017d68be74
ventuno <ernesto@fermi-4.local>
2014-11-17 17:59:39 -0800

    [Client] Introducing API versioning on client side
    Updating the URL so that it looks for the versioned version of the API.

--- ch-chr-client/app/scripts/services.js
+++ ch-chr-client/app/scripts/services.js
@@ -1,18 +1,18 @@
 var twitterServices = angular.module('twitterServices', ['ngResource']);
-
+var CHTWITTER_API_BASE_URL = '/api/twitter/1.0'
 twitterServices.factory('TwitterProfile', ['$resource',
 	function($resource){
-    	return $resource('/api/twitter/profile/:twitterhandle', {}, {});
+    	return $resource(CHTWITTER_API_BASE_URL + '/profile/:twitterhandle', {}, {});
  }]);
 
 twitterServices.factory('TwitterTimeline', ['$resource',
 	function($resource){
-    	return $resource('/api/twitter/timeline/:twitterhandle', {}, {
+    	return $resource(CHTWITTER_API_BASE_URL + '/timeline/:twitterhandle', {}, {
     		get: {method:'GET', isArray: true}
     	});
  }]);
 
 twitterServices.factory('TwitterReputationScore', ['$resource',
 	function($resource){
-    	return $resource('/api/twitter/reputation_score/:twitterhandle', {}, {});
+    	return $resource(CHTWITTER_API_BASE_URL + '/reputation_score/:twitterhandle', {}, {});
  }]);
