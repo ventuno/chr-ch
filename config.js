f421984551cae862f6f354411759eed59730fd5b
ventuno <ernesto@fermi-5.local>
2014-11-17 20:17:10 -0800

    [Backend] Uploading config.js file
    So the project can finally compile.

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	twitter : {
+		TWITTER_CUSTOMER_KEY: "IeF47NZ85O5oUEI7ame3w5C4P",
+		TWITTER_CUSTOMER_SECRET: "Fs4zMeJt5NdWgrRI0ogit3lLMJl4hkHAnNlvBPZuTHU9r21vbD",
+		MAX_TWEETS_COUNT: 10,
+		API_BASE_URL: '/api/twitter/1.0/'
+	}
+};
